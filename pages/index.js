3c9aff8d8a0063fdf8ff60961c36cb942484a0db
antritue <antritue@gmail.com>
2022-08-30 01:17:31 +0700

    Add title for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import Head from 'next/head';
 import { ThemeContext } from '../contexts/theme';
 import { GraphQLClient } from 'graphql-request';
 
@@ -26,30 +27,35 @@ export default function Home({ posts }) {
   const [{ themeName }] = useContext(ThemeContext);
 
   return (
-    <div id='top' className={`${themeName} app`}>
-      <Header />
-      <Introduction src='/blog-cover.jpg' />
+    <>
+      <Head>
+        <title>Kim Ngân - Câu chuyện trưởng thành</title>
+      </Head>
+      <div id='top' className={`${themeName} app`}>
+        <Header />
+        <Introduction src='/blog-cover.jpg' />
 
-      <main>
-        <div className='intro-title'>
-          <h1>Hi there</h1>
-          <h2>Welcome to my blog</h2>
-        </div>
-        <div className='cards__grid'>
-          {posts.map((post) => (
-            <BlogCard
-              title={post.title}
-              src={post.coverPhoto.url ? post.coverPhoto.url : ''}
-              alt={post.alt}
-              key={post.id}
-              slug={post.slug}
-            />
-          ))}
-        </div>
-      </main>
+        <main>
+          <div className='intro-title'>
+            <h1>Hi there</h1>
+            <h2>Welcome to my blog</h2>
+          </div>
+          <div className='cards__grid'>
+            {posts.map((post) => (
+              <BlogCard
+                title={post.title}
+                src={post.coverPhoto.url ? post.coverPhoto.url : ''}
+                alt={post.alt}
+                key={post.id}
+                slug={post.slug}
+              />
+            ))}
+          </div>
+        </main>
 
-      <ScrollToTop />
-      <Footer />
-    </div>
+        <ScrollToTop />
+        <Footer />
+      </div>
+    </>
   );
 }
